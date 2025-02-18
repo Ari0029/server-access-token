2509293cbc28e8067dd9c950c77ac4a670295398
chinh lox <2.12a12.chinhnguyenvan@gmail.com>
2025-02-18 21:34:20 +0700

    update1

--- controller/access.controller.js
+++ controller/access.controller.js
@@ -1,25 +1,26 @@
-const { GoogleAuth } = require("google-auth-library");
+const {GoogleAuth} = require("google-auth-library");
 const path = require("path");
 
 async function getAccessToken(req, res) {
-  const keyPath = path.join(
-    __dirname,
-    "/opt/render/project/src/json/project-app-65c58-3304fb741cef.json"
-  );
+    const keyPath = path.join(
+        __dirname,
+        "/etc/secrets/project-app-65c58-3304fb741cef.json"
+    );
 
-  const auth = new GoogleAuth({
-    keyFile: keyPath,
-    scopes: "https://www.googleapis.com/auth/firebase.messaging",
-  });
+    const auth = new GoogleAuth({
+        keyFile: keyPath,
+        scopes: "https://www.googleapis.com/auth/firebase.messaging",
+    });
 
-  try {
-    const client = await auth.getClient();
-    const accessToken = await client.getAccessToken();
-    console.log("Access Token:", accessToken.token);
-    res.json(accessToken.token);
-  } catch (error) {
-    console.error("Error getting access token:", error);
-    res.json({status: 500, message: "Null roif"});
-  }
+    try {
+        const client = await auth.getClient();
+        const accessToken = await client.getAccessToken();
+        console.log("Access Token:", accessToken.token);
+        res.json(accessToken.token);
+    } catch (error) {
+        console.error("Error getting access token:", error);
+        res.json({status: 500, message: `Null roif ${error}}`,});
+    }
 }
-module.exports = { getAccessToken };
+
+module.exports = {getAccessToken};
