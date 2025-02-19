73889632dce876f598d7fe85c8fedd6a639de63c
chinh lox <2.12a12.chinhnguyenvan@gmail.com>
2025-02-19 10:28:03 +0700

    update done access token

--- controller/access.controller.js
+++ controller/access.controller.js
@@ -1,24 +1,28 @@
-const { GoogleAuth } = require("google-auth-library");
+const {GoogleAuth} = require("google-auth-library");
 const path = require("path");
 
 async function getAccessToken(req, res) {
-  const keyPath = path.join(
-      "/etc/secrets/project-app-65c58-3304fb741cef.json" // Chỉ cần đường dẫn tuyệt đối
-  );
+    const keyPath = path.join(
+        "/etc/secrets/project-app-65c58-3304fb741cef.json" // Chỉ cần đường dẫn tuyệt đối
+    );
 
-  const auth = new GoogleAuth({
-    keyFile: keyPath,
-    scopes: "https://www.googleapis.com/auth/firebase.messaging",
-  });
+    const auth = new GoogleAuth({
+        keyFile: keyPath,
+        scopes: "https://www.googleapis.com/auth/firebase.messaging",
+    });
 
-  try {
-    const client = await auth.getClient();
-    const accessToken = await client.getAccessToken();
-    console.log("Access Token:", accessToken.token);
-    res.json(accessToken.token);
-  } catch (error) {
-    console.error("Error getting access token:", error);
-    res.json({status: 500, message: `Error: ${error.message}`});
-  }
+    try {
+        const client = await auth.getClient();
+        const accessToken = await client.getAccessToken();
+        console.log("Access Token:", accessToken.token);
+        res.json({
+            status: 200,
+            accessToken: accessToken.token,
+        });
+    } catch (error) {
+        console.error("Error getting access token:", error);
+        res.json({status: 500, message: `Error: ${error.message}`});
+    }
 }
-module.exports = { getAccessToken };
+
+module.exports = {getAccessToken};
